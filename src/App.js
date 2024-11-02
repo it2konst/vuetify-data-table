6e3433fe1c58a2b57aa314d8a57c3af2cb6f1563
it2konst <gorky17@tuta.io>
2024-11-02 18:14:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <header className="d-flex justify-between align-center p-40">
+                <div className="d-flex align-center">
+                    <img className="mr-15" src="./img/logo.webp" alt="Logo" width={40} height={40} />
+                    <div className="headerInfo">
+                        <h3 className="text-uppercase">React Sneakers</h3>
+                        <p>Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+                <ul className="d-flex">
+                    <li className="d-flex mr-30">
+                        <img className="mr-15" src="./img/cart.svg" alt="cart" width={18} height={18} />
+                        <span>1205 руб.</span>
+                    </li>
+                    <li>
+                        <img src="./img/user-svg.svg" alt="user" width={18} height={18} />
+                    </li>
+                </ul>
+            </header>
+
+            <div className="content p-40">
+                <h1>Все кроссовки</h1>
+                <div className="card"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
